bece7052fd9aa17f3343b88980f29a20d82cddcd
nishant.goyal <nishant.goyal@lalamove.com>
2020-11-06 13:15:04 +0530

    zero downtime

--- webserver-cluster/main.tf
+++ webserver-cluster/main.tf
@@ -18,6 +18,7 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
+  name = "${var.cluster_name}-${aws_launch_configuration.example.name}"
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
   target_group_arns    = [aws_lb_target_group.asg.arn]
@@ -26,6 +27,12 @@ resource "aws_autoscaling_group" "example" {
   min_size = var.min_size
   max_size = var.max_size
 
+  min_elb_capacity = var.min_size
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  
   tag {
     key                 = "Name"
     value               = "${var.cluster_name}-asg"
